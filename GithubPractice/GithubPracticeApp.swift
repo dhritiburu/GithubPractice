98d4d9c46c9fe5f72d5a61b93f8af117fc2d807c
Dhriti Burugupalli <burugupallidhriti@gmail.com>
2025-07-23 12:53:37 -0700

    Initial Commit

---
+++ GithubPractice/GithubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubPracticeApp.swift
+//  GithubPractice
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
