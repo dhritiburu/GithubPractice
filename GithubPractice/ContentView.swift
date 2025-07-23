c337990066fb8772ef2c89ec5bc008033aecce6f
Dhriti Burugupalli <burugupallidhriti@gmail.com>
2025-07-23 12:56:04 -0700

    Adding extra text

--- GithubPractice/ContentView.swift
+++ GithubPractice/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("practice 1")
+            Text("practice 2")
         }
         .padding()
     }
